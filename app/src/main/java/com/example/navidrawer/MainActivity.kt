16a7eee927042ccc598be731725376b9a1f4e408
AntiCaffe <dongjun6288@gmail.com>
2022-05-19 17:06:11 +0900

    plugin : android extensions Fragment add.

--- app/src/main/java/com/example/navidrawer/MainActivity.kt
+++ app/src/main/java/com/example/navidrawer/MainActivity.kt
@@ -6,15 +6,24 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentOne by lazy { FragmentOne() }
+    private val fragmentTwo by lazy { FragmentTwo() }
+
 
     lateinit var toggle : ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNavigationBottom()
+        changeFragment(fragmentOne) // 첫화면 설정
 
         val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
@@ -51,5 +60,28 @@ class MainActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun initNavigationBottom() {
+        bnv_main.run {
+            setOnNavigationItemSelectedListener {
+                when(it.itemId){
+                    R.id.first -> {
+                        changeFragment(fragmentOne)
+                    }
+                    R.id.second -> {
+                        changeFragment(fragmentTwo)
+                    }
+                }
+                true
+            }
+
+
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment)
+    {
+        supportFragmentManager.beginTransaction().replace(R.id.fl_container, fragment).commit()
     }
 }
